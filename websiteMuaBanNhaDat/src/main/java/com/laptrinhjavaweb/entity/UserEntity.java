2a38f33fe79c0fde44984a559532fa9dd955a183
Bachhop98 <bachhop9880@gmail.com>
2020-06-21 13:17:02 +0700

    update

--- websiteMuaBanNhaDat/src/main/java/com/laptrinhjavaweb/entity/UserEntity.java
+++ websiteMuaBanNhaDat/src/main/java/com/laptrinhjavaweb/entity/UserEntity.java
@@ -78,6 +78,38 @@ public class UserEntity extends BaseEntity {
 		this.fullName = fullName;
 	}
 
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	public Integer getStatus() {
 		return status;
 	}
